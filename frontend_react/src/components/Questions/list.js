cdc85b95a3d718775bd878605e2a458907605e39
alexahdp <alexahdp@gmail.com>
2018-09-05 23:17:54 +0300

    added filter in questions by tags

--- frontend_react/src/components/Questions/list.js
+++ frontend_react/src/components/Questions/list.js
@@ -1,13 +1,30 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import IconButton from '@material-ui/core/IconButton';
 import Chip from '@material-ui/core/Chip';
+import TextField from '@material-ui/core/TextField';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import styles from './assets/list.css';
 
-class App extends PureComponent {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filterTag: '',
+    };
+
+    this.setFilterTag = this.setFilterTag.bind(this);
+  }
+
+  setFilterTag(e) {
+    this.setState({
+      filterTag: e.target.value,
+    });
+  }
+
   render() {
     if (this.props.questions.size === 0) {
       return (
@@ -16,34 +33,49 @@ class App extends PureComponent {
     }
 
     return (
-      <List>
-        {this.props.questions.map(question => (
-          <ListItem divider={true} key={question.get('_id')} className={styles.listItem}>
-            <div className={styles.listItemBar}>
-              <div className={styles.listItemBarLeftSlot}>
-              {question.get('labels').map(label => (
-                <Chip key={label} label={label} className={styles.chip} />
-              ))}
-              </div>
-              <div className={styles.listItemBarRightSlot}>
-                <IconButton
-                  aria-label="Cart"
-                  onClick={() => this.props.editHandle(question.get('_id'))}
-                >
-                  <EditIcon />
-                </IconButton>
-                <IconButton
-                  aria-label="Cart"
-                  onClick={() => this.props.removeHandle(question.get('_id'))}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </div>
-            </div>
-            <p className={styles.text}>{question.get('text')}</p>
-          </ListItem>
-        ))}
-      </List>
+      <div>
+        <TextField
+          value={this.state.filterTag}
+          label="Tag filter"
+          fullWidth={true}
+          margin="normal"
+          onChange={this.setFilterTag}
+        />
+
+        <List>
+          {this.props.questions
+            .filter(question => {
+              if (this.state.filterTag === '') return true;
+              return question.get('labels').some(label => label.includes(this.state.filterTag));
+            })
+            .map(question => (
+              <ListItem divider={true} key={question.get('_id')} className={styles.listItem}>
+                <div className={styles.listItemBar}>
+                  <div className={styles.listItemBarLeftSlot}>
+                  {question.get('labels').map(label => (
+                    <Chip key={label} label={label} className={styles.chip} />
+                  ))}
+                  </div>
+                  <div className={styles.listItemBarRightSlot}>
+                    <IconButton
+                      aria-label="Cart"
+                      onClick={() => this.props.editHandle(question.get('_id'))}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton
+                      aria-label="Cart"
+                      onClick={() => this.props.removeHandle(question.get('_id'))}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </div>
+                </div>
+                <p className={styles.text}>{question.get('text')}</p>
+              </ListItem>
+          ))}
+        </List>
+      </div>
     );
   }
 }
